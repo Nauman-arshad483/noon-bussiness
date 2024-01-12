97ff57d416824164a355c2a07e26276597b8fced
Nauman-arshad483 <nauman.arshad483@gmail.com>
2024-01-12 21:32:36 +0500

    lint fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TrackSalesSection from "@/components/TrackSalesSection";
 
 export default function Home() {
   return (
-    <div className="w-full">
+    <div className='w-full'>
       <ManageGrowSection />
       <TrackSalesSection />
     </div>
